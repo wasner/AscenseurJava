5832a97d65a5e001534f4febe4b447e5ee1dcf49
Moumout <olivier.richit@hotmail.fr>
2015-12-26 23:56:12 +0100

    Update Etage.java

--- traitement/src/Etage.java
+++ traitement/src/Etage.java
@@ -6,14 +6,9 @@ import java.util.*;
  */
 public class Etage {
 
-    public Etage(String nomE, int numE) {
-    	nomEtage=nomE;
+    public Etage(int numE) {
     	numEtage=numE;
     }
 
     private int numEtage;
-    private String nomEtage;
-	public String getNomEtage() {
-		return nomEtage;
-	}
 }
