382b11b0b0c979aff27e4a61234a70862b43bb0a
Moumout <olivier.richit@hotmail.fr>
2015-12-26 20:36:53 +0100

    Update Ascenseur.java

--- traitement/src/Ascenseur.java
+++ traitement/src/Ascenseur.java
@@ -1,3 +1,6 @@
+package ascenseur.traitement;
+
+
 import java.util.*;
 
 /**
@@ -14,23 +17,24 @@ public class Ascenseur {
     
     private int nbEtagesDeservis;
     private int nbPersonneMax;
+    private Etage etageCourant;
     private int poidMax;
     private String etat;   					//immobileFerme, immobileOuvert, montant, descendant
     private boolean bloquer;
     
     private Utilisateur utilisateurs[];
-    private Etage etages[];					//liste des étages déservis. (exemple: 1,2,3,6,8,9)
+    private Etage etages[];					//liste des Ã©tages dÃ©servis. (exemple: 1,2,3,6,8,9)
     private Option options[];
     
     LinkedList<Requete> requetes=new LinkedList<Requete>();
 
     
-    public void bloquer(Ascenseur ascenseur) {
-    	this.bloquer=true;
+    public void bloquer() {
+    	bloquer=true;
     }
 
-    public void debloquer(Ascenseur ascenseur) {
-        this.bloquer=false;
+    public void debloquer() {
+        bloquer=false;
     }
 
     public void ajouterRequete(Requete requete) {
@@ -41,6 +45,38 @@ public class Ascenseur {
         Requete requete = new Requete(etage);
         ajouterRequete(requete);
     }
+     
+    
+    
+    
+    public void testEtatSuivantImmoFerme(){
+    	if((requetes.size()==0) || ((requetes.element()).getEtageAsc())!=etageCourant ){
+			etat="immobileFerme";
+    	}
+    }
+    
+    
+    public void testEtatSuivantImmoOuvert(){
+    	if((requetes.size()!=0) && ((requetes.element()).getEtageAsc())==etageCourant ){
+    		etat="immobileOuvert";
+    	}
+    }
+    
+    
+    public void testEtatSuivantMontant(){
+    	if((requetes.size()!=0) && ((requetes.element()).getEtageAsc())>etageCourant  ){
+    		etat="montant";
+    	}
+    }
+    
+    
+    public void testEtatSuivantDescendant(){
+    	if((requetes.size()!=0) && ((requetes.element()).getEtageAsc())<etageCourant  ){
+    		etat="descendant";
+    	}
+    }
+    
+    
 
     public void action() {
         // TODO implement here
