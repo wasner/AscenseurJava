5c37ae524762d4972f7df12e9a124a51f8461553
Moumout <olivier.richit@hotmail.fr>
2015-12-26 20:38:12 +0100

    Update Requete.java

--- traitement/src/Requete.java
+++ traitement/src/Requete.java
@@ -1,19 +1,12 @@
+package ascenseur.traitement;
 import java.util.*;
 
 /**
  * 
  */
 public class Requete {
-
-	private Etage etageAsc;
 	
-    public Requete(Etage etage) {
-    	etageAsc=etage;
+    public Requete() {
     }
-
-	public Etage getEtageAsc() {
-		return etageAsc;
-	}
-        
 
 }
