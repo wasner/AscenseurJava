5aeb0ed663d2f2e1d6b0fd906379a2b400819d38
jérémy <wasner83@gmail.com>
2015-12-27 10:56:39 +0100

    Modification de la classe requête en class abstraite pour que les classe requete Externe et interne en hérite

--- traitement/src/Requete.java
+++ traitement/src/Requete.java
@@ -3,17 +3,15 @@ import java.util.*;
 /**
  * 
  */
-public class Requete {
+public abstract class Requete
+{
+	public static int deplacement; //Variable qui contient l'état actuelle de l'ascenceur (en déplacement vers le haut, bas, arrêter, etc)
+	public static int monter; //L'ascenceur à reçu une requête pour monter
+	public static int descendre; //L'ascenceur à reçu une requpete pour descendre
+	public static int arret; //L'ascenceur à reçu une requête d'arrêt
+	public static int annuler; //La dernière commande envoyé à été annuler /!\ BONUS
 
-	private Etage etageAsc;
-	
-    public Requete(Etage etage) {
-    	etageAsc=etage;
-    }
+	public Requete() {
 
-	public Etage getEtageAsc() {
-		return etageAsc;
 	}
-        
-
 }
--- traitement/src/RequeteInterne.java
+++ traitement/src/RequeteInterne.java
@@ -9,6 +9,7 @@ public class RequeteInterne extends Requete {
      * Default constructor
      */
     public RequeteInterne() {
+        super();
     }
 
 
