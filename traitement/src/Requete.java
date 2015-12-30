192b6cfefd97e3ed415629666ae58ac6208cc665
Moumout <olivier.richit@hotmail.fr>
2015-12-30 10:58:57 +0100

    Update Requete.java

--- traitement/src/Requete.java
+++ traitement/src/Requete.java
@@ -1,3 +1,4 @@
+package ascenseur.traitement;
 import java.util.*;
 
 /**
@@ -5,11 +6,13 @@ import java.util.*;
  */
 public abstract class Requete
 {
-	public static int deplacement; //Variable qui contient l'état actuelle de l'ascenceur (en déplacement vers le haut, bas, arrêter, etc)
-	public static int monter; //L'ascenceur à reçu une requête pour monter
-	public static int descendre; //L'ascenceur à reçu une requpete pour descendre
-	public static int arret; //L'ascenceur à reçu une requête d'arrêt
-	public static int annuler; //La dernière commande envoyé à été annuler /!\ BONUS
+	Etage requeteEtage;
+	
+
+	public Etage getRequeteEtage() {
+		return requeteEtage;
+	}
+
 
 	public Requete() {
 	}
