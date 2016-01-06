2629c8ceb69c63519b0ae0e3fe05271068f391b2
lifesp <serramael@gmail.com>
2016-01-06 09:59:45 +0100

    Update Controleur.java

--- traitement/src/ascenseur/traitement/Controleur.java
+++ traitement/src/ascenseur/traitement/Controleur.java
@@ -1,4 +1,5 @@
-package ascenseur.traitement;
+package traitement;
+import java.util.*;
 
 /**
  * 
@@ -8,7 +9,11 @@ public class Controleur {
     /**
      * Default constructor
      */
-    public Controleur() {
+    public Controleur(int nbAscenseur, int nbEtage) {
+    	this.ascenseurs=new Ascenseur[nbAscenseur];
+    	Constante.setNbAscenseur(nbAscenseur);
+    	Constante.setNbEtage(nbEtage);
+    	this.requetes= new Vector<RequeteExterne>();
     }
 
     /**
@@ -19,7 +24,7 @@ public class Controleur {
     /**
      * 
      */
-    private RequeteExterne requetes[];
+    private Vector<RequeteExterne> requetes;
 
 
 
@@ -30,6 +35,7 @@ public class Controleur {
      */
     public void creerRequeteExterne(int etage, String direction) {
         // TODO implement here
+    	this.requetes.add(new RequeteExterne(etage, direction));
     }
 
     /**
@@ -37,6 +43,38 @@ public class Controleur {
      */
     public void choisirAscenseur(Ascenseur ascenseur) {
         // TODO implement here
+    	for(RequeteExterne r : this.requetes){
+    		Ascenseur ascenceurChoisie=null;
+    		for(Ascenseur a : this.ascenseurs){
+    			if(r.getDirection()==a.getEtat()){
+    				for (Etage e : a.getEtages()){
+    					if (etagesEgaux(e, r.getEtage())){
+    						if(r.getDirection()=="Dessend" && etageUnPlusPetit(r.getEtage(),a.getEtageCourant()){
+    							ascenceurChoisie=a;
+    						}
+    						else if (r.getDirection()=="Monter" && r.getEtage().compareTo(a.getEtageCourant())>0){
+    							ascenceurChoisie=a;
+    						}
+    					}
+    				}
+    			}
+    			else if (ascenceurChoisie==null && a.getEtat()=="Immobile Fermer"){
+    				ascenceurChoisie=a;
+    			}
+    		}
+    		ascenceurChoisie.ajouterRequete(r);
+    	}
+    }
+    private boolean etagesEgaux(Etage e1, Etage e2){
+		
+    	return e1.compareTo(e2)==0;
+    	
+    }
+    private boolean etageUnPlusPetit(Etage e1, Etage e2){
+    	return e1.compareTo(e2)<0;
+    }
+    private boolean etageUnPlusGrand(Etage e1, Etage e2){
+    	return e1.compareTo(e2)>0;
     }
 
 }
