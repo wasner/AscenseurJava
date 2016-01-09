bb7f139db5b4aae3fd153ec58db5be5db62e40c9
Maelou04 <thomasseymael@gmail.com>
2016-01-09 20:02:56 +0100

    Update Option3.java

--- ascenseur/src/traitement/Option3.java
+++ ascenseur/src/traitement/Option3.java
@@ -1,14 +1,7 @@
-package traitement;
+package ascenceur.traitement;
 
-/**
- * 
- */
 public class Option3 implements Option {
 
-    /**
-     * Default constructor
-     */
     public Option3() {
     }
-
 }
