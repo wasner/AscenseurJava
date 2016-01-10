5461f31b7a1279fb1d9dea637d8d9980e4d650a3
Maelou04 <thomasseymael@gmail.com>
2016-01-10 22:20:27 +0100

    Update and rename Test.java to Interface.java
    First upload

---
+++ ascenseur/src/affichage/Interface.java
@@ -0,0 +1,11 @@
+package ascenceur.affichage;
+
+public class Interface {
+	
+	@SuppressWarnings("unused")
+	private static Window window = null;
+	
+	public static void main(String[] args){
+		window = new Window();
+	}       
+}
