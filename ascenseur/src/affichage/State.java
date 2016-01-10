6a8b19b73fa6fd9bc5fa0304435cd350e6c85ac6
Maelou04 <thomasseymael@gmail.com>
2016-01-10 22:20:57 +0100

    Update and rename a.java to State.java
    First upload

---
+++ ascenseur/src/affichage/State.java
@@ -0,0 +1,22 @@
+package ascenceur.affichage;
+
+public class State {
+	private InitState initState;
+	private ParamState paramState;
+	
+	public State(Window window)
+	{
+		this.initState = new InitState(window, this);
+		this.paramState = new ParamState(window, this);
+	}
+	
+	public InitState getInitState()
+	{
+		return this.initState;
+	}
+	
+	public ParamState getParamState()
+	{
+		return this.paramState;
+	}
+}
