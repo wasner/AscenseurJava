f45ec3b676a4fee917d1c6f4312103332eb908ae
jérémy <wasner83@gmail.com>
2016-01-17 20:35:38 +0100

    Modification ascenseur test PS : il faut modifier aussi la fonction trieAppel car j'ajoute deux fois les mêmes requetes

--- ascenseur/test/AscenseurTest.java
+++ ascenseur/test/AscenseurTest.java
@@ -30,25 +30,37 @@ public class AscenseurTest {
         Etage r ;
         for(int i =0; i < 10; ++i){ etages.add(new Etage(i)); }
 
-        Etage etageCourant = ascenseur1.getEtageCourant();
+        Etage etageCourant = new Etage(5);
+        ascenseur1.setEtageCourant(etageCourant);
 
-        List<Requete> requetes = new LinkedList<Requete>();
-        for(int i = 0; i < 5; ++i){
+
+        ascenseur1.ajouterRequete(new RequeteExterne("montant",etages.get(1)));
+        ascenseur1.ajouterRequete(new RequeteExterne("montant",etages.get(8)));
+        ascenseur1.ajouterRequete(new RequeteExterne("montant",etages.get(9)));
+        ascenseur1.ajouterRequete(new RequeteExterne("montant",etages.get(5)));
+        ascenseur1.ajouterRequete(new RequeteExterne("montant",etages.get(4)));
+        ascenseur1.ajouterRequete(new RequeteExterne("montant",etages.get(6)));
+
+        List<Requete> requetes = ascenseur1.getRequetes();
+
+
+        /*for(int i = 0; i < 5; ++i){
             requetes.add(new RequeteExterne("montant",etages.get(i)));
-            System.out.println(requetes.get(i).getEtage());
+            //System.out.println(requetes.get(i).getEtage());
         }
-        for(int i = 0; i < 5; ++i){
-            requetes.add(new RequeteExterne("descendant",etages.get(i+5)));
-            System.out.println(i+5);
-        }
-        System.out.println(requetes.size());
+        for(int j = 0; j < 5; ++j){
+            requetes.add(new RequeteExterne("descendant",etages.get(j+5)));
+            System.out.println("Descendant : " + j+5);
+        }*/
+        System.out.println("Nombres de requêtes : " + requetes.size());
+        System.out.println("Liste des requêtes non triées : " );
         for(Requete req: requetes)
         {
-
-            System.out.println(req.getEtage());
+            System.out.println( req.getEtage().getNumEtage());
         }
         ascenseur1.triAppel();
         System.out.println("Liste des requêtes triés :");
+        //System.out.println(requetes.get(1).getEtage().getNumEtage());
         for(Requete req: requetes)
         {
             System.out.println(req.getEtage().getNumEtage());
