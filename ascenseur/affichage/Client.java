17cc5add896a839f50a21975b3ac0d0e68fd26ea
wasner <wasner83@gmail.com>
2016-01-18 15:01:53 +0100

    Update Client.java

--- ascenseur/affichage/Client.java
+++ ascenseur/affichage/Client.java
@@ -1,34 +1,26 @@
-package ascenseur.affichage;
+package affichage;
 
-import java.awt.Frame;
 import java.util.LinkedList;
 import java.util.List;
 
-import ascenseur.traitement.Ascenseur;
-import ascenseur.traitement.Constante;
-import ascenseur.traitement.Etage;
-import ascenseur.traitement.Immeuble;
+import traitement.Ascenseur;
+import traitement.Constante;
+import traitement.Controleur;
+import traitement.Etage;
+import traitement.Immeuble;
 
 public class Client {
 
 	public static void main(String[] args){
-		
-		
-		
-		
-		
-		
-		String nom = "Name";
-		List<Etage> etages = new LinkedList<Etage>();
-		while(true)
-		{
-			new Frame();
 
-		}
-		/*for(int i =0; i < 5; i++)
+		String nom = "Name";
+
+		List<Etage> etages = new LinkedList<Etage>();
+		for(int i =0; i < Constante.getNbEtage(); i++)
 			etages.add(new Etage(i));
 		List<Ascenseur> ascenseurs = new LinkedList<Ascenseur>();
-		Immeuble immeuble = new Immeuble(nom, etages, ascenseurs);
-		immeuble.addVue(new VueInit(immeuble));*/
-	}       
+		Controleur co =new Controleur(ascenseurs);
+		Immeuble immeuble = new Immeuble(nom, etages, ascenseurs,co);
+		immeuble.addVue(new VueInit(immeuble));
+	}
 }
